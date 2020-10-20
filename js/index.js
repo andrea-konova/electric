167fa30afc68b74e9aa8f59247dcb4035d01a9a2
Andrii Konovalenko <an256.ak@gmail.com>
2020-10-21 01:00:36 +0300

    feat: created animation

--- js/index.js
+++ js/index.js
@@ -2,16 +2,14 @@ import { burgerMenu } from './modules/burgerMenu.js';
 import { smoothScroll } from './modules/smoothScroll.js';
 import { addProjects } from './modules/addProjects.js';
 import { popup } from './modules/popup.js';
-// import { popupSlider } from './modules/popupSlider.js';
 import { feedbackSlider } from './modules/feedbackSlider.js';
 import { sendForms } from './modules/sendForms.js';
 
-// new WOW().init();
+new WOW().init();
 
 burgerMenu();
 smoothScroll();
 addProjects();
 popup();
 feedbackSlider();
-// popupSlider();
 sendForms();
